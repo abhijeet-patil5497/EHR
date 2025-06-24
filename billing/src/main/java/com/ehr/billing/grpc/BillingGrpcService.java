cffb4f6b8f1bb55bacdcc749f0481cfa5342744b
abhijeet-patil5497 <abhipatil5497@gmail.com>
2025-06-24 11:22:37 +0530

    Implemented Billing Service

---
+++ billing/src/main/java/com/ehr/billing/grpc/BillingGrpcService.java
@@ -0,0 +1,26 @@
+package com.ehr.billing.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("Active")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+}
